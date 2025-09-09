da44837dda6cf1b06abec076bb60fa507989c6c6
Vaibhav123-dotcom <143591012+Vaibhav123-dotcom@users.noreply.github.com>
2025-09-09 11:58:20 +0530

    added

--- server/utils/db.js
+++ server/utils/db.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 async function connectTODb(){
     try {
-        await mongoose.connect(process.env.MONGO_URL + "blog-detial")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log(">>>>>>>>>>>>>>>>>>>> Connected To Database <<<<<<<<<<<<<<<<<<<<")
         // console.log(`Server is connected to ${process.env.MONGO_URL + "blog-detail"}`)
     } catch (error) {
